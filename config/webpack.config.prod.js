8733dbcdda4223714e5d6e269a62255fc86612c9
Dave Weijgertse <dave.weijgertse@gmail.com>
2017-11-26 22:31:59 +0100

    Include Bootstrap v4 in the webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -364,6 +364,13 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // Add Boostrap v4 support
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+    }),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
