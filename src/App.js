e4971a7d95bd4af4e9ee2221b5afe6dd5d2ed7f1
Dave Weijgertse <dave.weijgertse@gmail.com>
2017-12-12 21:12:43 +0100

    <HeaderBar>: Include it in the <App>-template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import HeaderBar from './Components/Header/HeaderBar';
 import PageLanding from './Pages/Landing/PageLanding';
 
 import 'bootstrap';
@@ -11,6 +12,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
+                <HeaderBar />
                 <div className="container">
                     <Router>
                         <Route path="/"
