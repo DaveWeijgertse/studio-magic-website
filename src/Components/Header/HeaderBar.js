9d4078d9d79df411f7e9f3677e682fe0e23b186e
Dave Weijgertse <dave.weijgertse@gmail.com>
2017-12-12 21:14:45 +0100

    <HeaderBar>: Add the Studio Magic logo

--- src/Components/Header/HeaderBar.js
+++ src/Components/Header/HeaderBar.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 import './HeaderBar.scss';
+import logo from './logo.png';
 
 class HeaderBar extends React.Component {
     render() {
         return (
             <div className="header-bar">
+                <img
+                    className="logo"
+                    alt="Studio Magic"
+                    src={logo}
+                />
             </div>
         )
     }
