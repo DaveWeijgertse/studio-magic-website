541589650a2c4838e817184c89e31349b93fc8bf
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 14:07:15 +0100

    <PayOffText>: Add the pay-off text

--- src/Pages/Landing/Subcomponents/PayOffText/PayOffText.js
+++ src/Pages/Landing/Subcomponents/PayOffText/PayOffText.js
@@ -3,7 +3,8 @@ import i18n from 'i18n-react';
 
 
 const PayOffText = () => (
-    <div>
+    <div className="pay-off-text-container">
+        <div>{i18n.translate('pay-off')}</div>
     </div>
 );
 
