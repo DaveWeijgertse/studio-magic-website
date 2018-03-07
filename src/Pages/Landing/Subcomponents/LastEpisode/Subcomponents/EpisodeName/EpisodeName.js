7021e6ff03caf32cfa16491ae220542f890d454b
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 15:34:47 +0100

    Add styling to the name in the <EpisodeName>-component

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/EpisodeName/EpisodeName.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/EpisodeName/EpisodeName.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+// Styling imports
+import './EpisodeName.scss';
+
 
 const EpisodeName = () => (
-    <div>
-
+    <div className="episode-name">
         Jace, the Mind Sculptor: Beter dan alles?
     </div>
 );
