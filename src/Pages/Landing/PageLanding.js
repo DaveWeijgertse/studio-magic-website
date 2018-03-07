3202520310f632188ccfb2304b89f8ef9ad32f56
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 16:58:29 +0100

    Pass the data of the last episode down to the <LastEpisode>-component

--- src/Pages/Landing/PageLanding.js
+++ src/Pages/Landing/PageLanding.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import {
+    last,
+} from 'lodash';
+
 // Subcomponent imports
 import LastEpisode from './Subcomponents/LastEpisode';
 import PayOffText from './Subcomponents/PayOffText';
@@ -12,11 +16,25 @@ import './PageLanding.scss';
 
 
 class PageLanding extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lastEpisode: last(episodes)
+        }
+    }
+
     render() {
+        const {
+            lastEpisode,
+        } = this.state;
+
         return (
             <div>
                 <PayOffText />
-                <LastEpisode />
+                <LastEpisode
+                    lastEpisode={lastEpisode}
+                />
             </div>
         )
     }
