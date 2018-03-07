d2dd3391a025bca13e74ccb872fceffba8ddc5b0
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 17:22:53 +0100

    <EpisodeNumber>: Add formatter for the episode number

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/EpisodeNumberAndDate/Subcomponents/EpisodeNumberLabel/EpisodeNumberLabel.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/EpisodeNumberAndDate/Subcomponents/EpisodeNumberLabel/EpisodeNumberLabel.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import i18n from 'i18n-react';
 
+// Formatter import
+import formatEpisodeNumber from './formatters/formatEpisodeNumber';
+
 // Styling import
 import './EpisodeNumberLabel.scss';
 
 
 const EpisodeNumberLabel = ({number}) => (
     <div className="episode-number-label">
-        {i18n.translate('episode.singular') + " #" + number.toString()}
+        {i18n.translate('episode.singular') + formatEpisodeNumber(number)}
     </div>
 );
 
---
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/EpisodeNumberAndDate/Subcomponents/EpisodeNumberLabel/formatters/formatEpisodeNumber.js
@@ -0,0 +1,15 @@
+export default function formatEpisodeNumber(number) {
+    const hashtag        = " #";
+    const numberAsString = number.toString();
+    const length         = numberAsString.length;
+
+    if (length === 1) {
+        return hashtag + "00" + numberAsString;
+    } else if (length === 2) {
+        return hashtag + "0" + numberAsString;
+    } else if (length === 3) {
+        return hashtag + numberAsString;
+    }
+
+    return " ???";
+}
