59c01d4eebdf7353a8016f7998b919f909b0b171
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 15:26:07 +0100

    Include the <EpisodeDate>-component in the <EpisodeNumberAndDate>

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/EpisodeNumberAndDate/EpisodeNumberAndDate.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/EpisodeNumberAndDate/EpisodeNumberAndDate.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 // Subcomponent imports
+import EpisodeDate from './Subcomponents/EpisodeDate';
 import EpisodeNumberLabel from './Subcomponents/EpisodeNumberLabel';
 
 
 const EpisodeNumberAndDate = () => (
     <div>
         <EpisodeNumberLabel />
+        <EpisodeDate />
     </div>
 );
 
