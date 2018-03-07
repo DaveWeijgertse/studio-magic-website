352a23b8a6393d0283cd5bc50db1867b752f6056
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 14:07:30 +0100

    <PayOffText>: Add styling

--- src/Pages/Landing/Subcomponents/PayOffText/PayOffText.js
+++ src/Pages/Landing/Subcomponents/PayOffText/PayOffText.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import i18n from 'i18n-react';
 
+// Styling imports
+import './PayOffText.scss';
+
 
 const PayOffText = () => (
     <div className="pay-off-text-container">
