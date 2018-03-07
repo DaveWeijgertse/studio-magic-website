5aaea62935307734b54e20d13897199ba0f8af13
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 19:33:26 +0100

    <PlayButton>: Add styling

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/ExtraTopicsAndPlayButton/Subcomponents/PlayButton/PlayButton.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/ExtraTopicsAndPlayButton/Subcomponents/PlayButton/PlayButton.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import i18n from 'i18n-react';
 
+// Styling imports
+import './PlayButton.scss';
+
 
 const PlayButton = ({url}) => (
     <a
+        className="play-button"
         href={url}
         target="_blank"
     >
