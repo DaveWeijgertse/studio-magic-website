f5f34252f6e4e0f8bc03b49b86f993364738bb35
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 21:42:32 +0100

    Include the episode data in the <PageEpisodesList> and render an empty <EpisodeTile> for each episode

--- src/Pages/EpisodesList/PageEpisodesList.js
+++ src/Pages/EpisodesList/PageEpisodesList.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
+// Data imports
+import { episodes } from 'appUtils/episodes/episodes.json';
+
+// Subcomponent imports
+import EpisodeTile from './Subcomponents/EpisodeTile';
+
 
 class PageEpisodesList extends React.Component {
     render() {
         return (
             <div>
+                <div className="all-episode-container">
+                    {episodes.map(
+                        (episode) => {
+                            const {
+                                id,
+                            } = episode;
 
+                            return (
+                                <EpisodeTile
+                                    key={id}
+                                />
+                            );
+                        }
+                    )}
+                </div>
             </div>
         )
     }
