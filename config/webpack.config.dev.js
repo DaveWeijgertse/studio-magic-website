e02e81b4e9aaa6df9b367e143ede24e067d87706
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 13:41:09 +0100

    Add alias to the /utils folder for both dev and production

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -88,6 +88,9 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+
+      // Paths to .scss and .sass files elsewhere in the project tree
+      'appUtils': paths.appUtils,
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
