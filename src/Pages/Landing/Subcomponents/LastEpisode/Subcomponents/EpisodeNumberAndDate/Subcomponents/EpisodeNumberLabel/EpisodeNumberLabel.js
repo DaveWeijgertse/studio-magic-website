df39f7337aad9a4e9c3c01f0555e584d0049b8b6
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 15:21:50 +0100

    <EpisodeNumberLabel>: Add the epsiode + latest episode number

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/EpisodeNumberAndDate/Subcomponents/EpisodeNumberLabel/EpisodeNumberLabel.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/EpisodeNumberAndDate/Subcomponents/EpisodeNumberLabel/EpisodeNumberLabel.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import i18n from 'i18n-react';
+
+
 
 const EpisodeNumberLabel = () => (
-    <div>
-
+    <div className="episode-number-label">
+        {i18n.translate('episode.singular')} #009
     </div>
 );
 
