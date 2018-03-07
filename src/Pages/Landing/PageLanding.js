7bfa89013a4dd39490bbe534ec5ae60d7bfad9c0
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 14:08:19 +0100

    Remove the <iframe> to Buzzsprout

--- src/Pages/Landing/PageLanding.js
+++ src/Pages/Landing/PageLanding.js
@@ -6,12 +6,7 @@ import './PageLanding.scss';
 class PageLanding extends React.Component {
     render() {
         return (
-            <div className="buzzsprout-container">
-                <iframe
-                    className="buzzsprout"
-                    title="Studio Magic op Buzzsprout"
-                    src="http://www.buzzsprout.com/133608"
-                />
+            <div>
             </div>
         )
     }
