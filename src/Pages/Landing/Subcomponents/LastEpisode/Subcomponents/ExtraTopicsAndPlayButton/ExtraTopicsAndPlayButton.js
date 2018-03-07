371f715c5f5bca419731cd4543cced641549b64c
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-07 18:56:45 +0100

    Include the <PlayButton> in the <ExtraTopicsAndPlayButton> parent component

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/ExtraTopicsAndPlayButton/ExtraTopicsAndPlayButton.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/ExtraTopicsAndPlayButton/ExtraTopicsAndPlayButton.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 // Subcomponent imports
 import ExtraTopics from './Subcomponents/ExtraTopics';
+import PlayButton from './Subcomponents/PlayButton';
 
 
 const ExtraTopicsAndPlayButton = ({extraTopics}) => (
     <div className="extra-topics-and-play-button-container">
         <ExtraTopics extraTopics={extraTopics} />
+        <PlayButton />
     </div>
 );
 
