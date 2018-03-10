8ee3a91836a3f8a4ea787147b5645d784e243d36
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 13:26:01 +0100

    Include the <AiringDate> to the <Info> in the <EpisodeTile>

--- src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/Info/Info.js
+++ src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/Info/Info.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 // Subcomponent imports
+import AiringDate from './Subcomponents/AiringDate';
 import EpisodeImage from '../../../../../../Components/EpisodeImage';
 
 // Styling imports
 import './Info.scss';
 
 
-const Info = ({id}) => (
+const Info = ({id, date}) => (
     <div className="episode-tile-info">
-
+        <AiringDate
+            date={date}
+        />
         <EpisodeImage
             episodeNumber={id}
         />
