3044ecea0741b919f02b92a3dcc30d52c07e0abd
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 19:20:45 +0100

    Add helper to get the correct URL based on the podcast source type

---
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/utils/getPodcastSourceHref.js
@@ -0,0 +1,15 @@
+import {
+    ITUNES,
+    STITCHER,
+    TUNEIN,
+} from './podcastSourceTypes';
+
+export default function getPodcastSourceHref(type) {
+    const podcastSourceHref = {
+        [ITUNES]:   "https://itunes.apple.com/us/podcast/studio-magic/id1309473851",
+        [STITCHER]: "https://www.stitcher.com/podcast/studio-magic",
+        [TUNEIN]:   "https://tunein.com/radio/Studio-Magic-p1085711"
+    }
+
+    return getPodcastSourceHref[type];
+}
