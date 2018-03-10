1ac7b9dac4f0d62ea9d7da6886e3625c4b58a7a2
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 22:31:47 +0100

    Only render the content in <EpisodePlayerBar> if an id has been selected

--- src/Components/EpisodePlayerBar/EpisodePlayerBar.js
+++ src/Components/EpisodePlayerBar/EpisodePlayerBar.js
@@ -3,8 +3,18 @@ import React from 'react';
 
 class EpisodePlayerBar extends React.Component {
     render() {
+        const {
+            id,
+        } = this.props;
+
+        const isPlayingEpisode = typeof id !== 'undefined' && id !== null;
+
         return (
             <div className="episode-player-bar-container">
+                {isPlayingEpisode ? (
+                    <div className="episode-player-bar">
+                    </div>
+                ) : null}
             </div>
         )
     }
