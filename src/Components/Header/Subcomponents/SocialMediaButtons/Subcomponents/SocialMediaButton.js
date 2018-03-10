4f25da426fffa3c1f8d2a17961d0e32d5c67ca05
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 15:35:48 +0100

    Refactor the <SocialMediaButtons> styling to display them as an icon

--- src/Components/Header/Subcomponents/SocialMediaButtons/Subcomponents/SocialMediaButton.js
+++ src/Components/Header/Subcomponents/SocialMediaButtons/Subcomponents/SocialMediaButton.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 // Util imports
 import getSocialMediaHref from '../utils/getSocialMediaHref';
-import getSocialMediaIcon from '../utils/getSocialMediaIcon';
 
 // Styling import
 import './SocialMediaButton.scss';
@@ -13,14 +12,10 @@ class SocialMediaButton extends React.Component {
         const { type } = this.props;
 
         return (
-            <a className={"social-media-button " + type}
-               href={getSocialMediaHref(type)}
-            >
-                <img
-                    src={getSocialMediaIcon(type)}
-                    alt={type}
-                />
-            </a>
+            <a
+                className={"social-media-button " + type}
+                href={getSocialMediaHref(type)}
+            />
         );
     }
 }
