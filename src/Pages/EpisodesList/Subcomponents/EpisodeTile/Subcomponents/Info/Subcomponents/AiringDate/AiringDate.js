cf2b965ad0e83e054680281a636a8ac3caca70e4
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 13:25:46 +0100

    Add the formatted date to the <AiringDate>

--- src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/Info/Subcomponents/AiringDate/AiringDate.js
+++ src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/Info/Subcomponents/AiringDate/AiringDate.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// Formatters import
+import formatDate from 'appUtils/formatters/formatDate';
 
-const AiringDate = () => (
+
+
+const AiringDate = ({date}) => (
     <div className="episode-tile-info-airing-date">
-
+        {formatDate(date)}
     </div>
 );
 
