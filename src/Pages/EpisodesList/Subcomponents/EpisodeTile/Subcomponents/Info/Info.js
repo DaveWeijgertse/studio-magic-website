716aae648dc7b51ee5c098659f8f743a15eae639
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 13:28:22 +0100

    Include the <EpisodeNumber> in the <Info> of the <EpisodeTile>

--- src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/Info/Info.js
+++ src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/Info/Info.js
@@ -3,6 +3,7 @@ import React from 'react';
 // Subcomponent imports
 import AiringDate from './Subcomponents/AiringDate';
 import EpisodeImage from '../../../../../../Components/EpisodeImage';
+import EpisodeNumber from './Subcomponents/EpisodeNumber';
 
 // Styling imports
 import './Info.scss';
@@ -12,6 +13,9 @@ const Info = ({id, date}) => (
     <div className="episode-tile-info">
         <AiringDate
             date={date}
+        />
+        <EpisodeNumber
+            number={id}
         />
         <EpisodeImage
             episodeNumber={id}
