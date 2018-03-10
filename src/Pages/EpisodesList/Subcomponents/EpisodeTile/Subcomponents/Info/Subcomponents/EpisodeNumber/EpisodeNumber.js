b7dcd474d46afeb11eb3c6a0a296b192c3a967bf
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 13:29:10 +0100

    Add styling to the <EpisodeNumber>

--- src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/Info/Subcomponents/EpisodeNumber/EpisodeNumber.js
+++ src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/Info/Subcomponents/EpisodeNumber/EpisodeNumber.js
@@ -4,6 +4,8 @@ import i18n from 'i18n-react';
 // Formatter import
 import formatEpisodeNumber from 'appUtils/formatters/formatEpisodeNumber';
 
+// Styling import
+import './EpisodeNumber.scss';
 
 
 const EpisodeNumber = ({number}) => (
