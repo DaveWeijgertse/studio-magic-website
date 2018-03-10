5ff64a87cd5759b93bc532416d35e54cbf1a1c19
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 12:51:54 +0100

    Move the formatDate() to appUtils/formatters

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/EpisodeNumberAndDate/Subcomponents/EpisodeDate/EpisodeDate.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/EpisodeNumberAndDate/Subcomponents/EpisodeDate/EpisodeDate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Formatters import
-import formatDate from './formatters/formatDate';
+import formatDate from 'appUtils/formatters/formatDate';
 
 // Styling import
 import './EpisodeDate.scss';
