2bba05323f278763e475d1a7b7c8a0c327d28b7e
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 00:34:23 +0100

    Fix bug where the formatTimeFromSeconds() attempted to render NaN values

--- src/Components/EpisodePlayerBar/Subcomponents/TimerDisplay/utils/formatTimeFromSeconds.js
+++ src/Components/EpisodePlayerBar/Subcomponents/TimerDisplay/utils/formatTimeFromSeconds.js
@@ -1,3 +1,8 @@
+import {
+    isNaN,
+    isNull,
+} from 'lodash';
+
 export default function formatTimeFromSeconds(secs) {
     let seconds = secs;
     let minutes = Math.floor(secs / 60);
@@ -9,11 +14,12 @@ export default function formatTimeFromSeconds(secs) {
 }
 
 function formatTimePart(timePart) {
-    const roundedTimePart = Math.round(timePart);
+    const roundedTimePart  = Math.round(timePart);
+    const hasNoTimePartYet = isNull(roundedTimePart) || isNaN(roundedTimePart);
 
     // Add a 0 in front of the timePart, if it's only a single number
     // Return "00" if there is no timePart yet
-    if (roundedTimePart === null) {
+    if (hasNoTimePartYet) {
         return "00";
     } else if (roundedTimePart >= 0 && roundedTimePart <= 9) {
         return "0" + roundedTimePart.toString();
