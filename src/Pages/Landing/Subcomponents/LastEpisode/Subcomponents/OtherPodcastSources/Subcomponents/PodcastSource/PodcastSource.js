71f94313bba8bb34f9a7c432a631b970eba22d3a
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 19:36:16 +0100

    Render the icon of the podcast source (as a link) in the <PodcastSource>

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/Subcomponents/PodcastSource/PodcastSource.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/Subcomponents/PodcastSource/PodcastSource.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+// Utils import
+import getPodcastSourceHref from '../../utils/getPodcastSourceHref';
+import getPodcastSourceIcon from '../../utils/getPodcastSourceIcon';
 
-const PodcastSource = ({url}) => (
+
+const PodcastSource = ({source}) => (
     <div className="podcast-source-container">
+        <a
+            href={getPodcastSourceHref(source)}
+            target="_blank"
+        >
+            <img
+                alt={source}
+                src={getPodcastSourceIcon(source)}
+            />
+        </a>
     </div>
 );
 
