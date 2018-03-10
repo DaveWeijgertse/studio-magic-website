936b3521066c77c748752bc8129fc68d4ea34fc0
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 23:29:08 +0100

    Display an pause icon when playing and a play icon when paused in the <PlayPauseButton>

--- src/Components/EpisodePlayerBar/Subcomponents/PlayPauseButton/PlayPauseButton.js
+++ src/Components/EpisodePlayerBar/Subcomponents/PlayPauseButton/PlayPauseButton.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+// Styling import
+import './PlayPauseButton.scss';
 
 
-const PlayPauseButton = ({onClick}) => (
+const PlayPauseButton = ({isPlaying, onClick}) => (
     <div
+        className={"play-pause-button " + (isPlaying ? "playing" : "paused")}
         onClick={onClick}
     />
 );
