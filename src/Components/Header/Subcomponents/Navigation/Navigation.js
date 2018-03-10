0f00ff264b7ad061fa8a15a32fe23349038ef200
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 16:09:45 +0100

    Add a link to the <PageEpisodesList> in the <Navigation>

--- src/Components/Header/Subcomponents/Navigation/Navigation.js
+++ src/Components/Header/Subcomponents/Navigation/Navigation.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import i18n from 'i18n-react';
+import { Link } from 'react-router-dom';
+
+import {
+    ROUTE_EPISODES_LIST,
+} from 'appUtils/routes/paths';
+
 
 
 const Navigation = () => (
     <div className="navigation-container">
+        <Link to={ROUTE_EPISODES_LIST}>
+            {i18n.translate('episode.plural')}
+        </Link>
     </div>
 );
 
