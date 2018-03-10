07dd3aa35be82ed4ef9671acc891c88a988d5ebc
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 00:52:35 +0100

    Display the episode number and name in the <EpisodeInfo>

--- src/Components/EpisodePlayerBar/Subcomponents/EpisodeInfo/EpisodeInfo.js
+++ src/Components/EpisodePlayerBar/Subcomponents/EpisodeInfo/EpisodeInfo.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import i18n from 'i18n-react';
+
+// Utils imports
+import formatEpisodeNumber from 'appUtils/formatters/formatEpisodeNumber';
 
 
-const EpisodeInfo = () => (
+
+const EpisodeInfo = ({id, name}) => (
     <div className="episode-info">
+        <div className="episode-info-number">
+            {i18n.translate('episode.singular') + " " + formatEpisodeNumber(id)}
+        </div>
+        <div className="episode-info-name">{name}</div>
     </div>
 );
 
