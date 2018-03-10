f05b967f65d1d598262400a6aaa6b613124b674e
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 22:21:05 +0100

    Add support to the routing to accept props

--- src/App.js
+++ src/App.js
@@ -24,6 +24,21 @@ import 'bootstrap';
 import './App.scss';
 
 
+const renderMergedProps = (component, ...rest) => {
+    const finalProps = Object.assign({}, ...rest);
+    return (
+        React.createElement(component, finalProps)
+    );
+}
+
+const PropsRoute = ({ component, ...rest }) => {
+    return (
+        <Route {...rest} render={routeProps => {
+            return renderMergedProps(component, routeProps, rest);
+        }}/>
+    );
+}
+
 class App extends React.Component {
     componentWillMount() {
         i18n.setTexts(NL);
