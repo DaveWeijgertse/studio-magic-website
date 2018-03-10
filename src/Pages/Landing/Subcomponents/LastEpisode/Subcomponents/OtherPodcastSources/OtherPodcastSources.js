932b77b748551ef81e56ee38f323a04925727d98
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 19:38:13 +0100

    Add the <PodcastSource> for iTunes, Stitcher and TuneIn

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/OtherPodcastSources.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/OtherPodcastSources.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import {
+    ITUNES,
+    STITCHER,
+    TUNEIN,
+} from './utils/podcastSourceTypes';
+
+// Subcomponents import
+import PodcastSource from './Subcomponents/PodcastSource';
+
 // Styling import
 import './OtherPodcastSources.scss';
 
@@ -7,6 +16,13 @@ import './OtherPodcastSources.scss';
 const OtherPodcastSources = () => {
     return (
         <div className="other-podcast-sources-container">
+            <div className="other-podcast-sources">
+                <div className="podcast-sources-container">
+                    <PodcastSource source={ITUNES} />
+                    <PodcastSource source={STITCHER} />
+                    <PodcastSource source={TUNEIN} />
+                </div>
+            </div>
         </div>
     );
 }
