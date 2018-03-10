acfc7fbf91a38ae426e1233c757570785df904e4
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 17:22:36 +0100

    Add styling for <MoreInfo>

--- src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/MoreInfo/MoreInfo.js
+++ src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/MoreInfo/MoreInfo.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import i18n from 'i18n-react';
 
+// Styling import
+import './MoreInfo.scss';
 
 
 const MoreInfo = ({url}) => (
     <a
+        className="episode-more-info"
         href={url}
         target="_blank"
     >
