1f01674ac517aa9a744f6653e7d6c7d30ae63276
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-10 13:30:01 +0100

    Include the <Name> in the <Info> of the <EpisodeTile>

--- src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/Info/Info.js
+++ src/Pages/EpisodesList/Subcomponents/EpisodeTile/Subcomponents/Info/Info.js
@@ -4,18 +4,22 @@ import React from 'react';
 import AiringDate from './Subcomponents/AiringDate';
 import EpisodeImage from '../../../../../../Components/EpisodeImage';
 import EpisodeNumber from './Subcomponents/EpisodeNumber';
+import Name from './Subcomponents/Name';
 
 // Styling imports
 import './Info.scss';
 
 
-const Info = ({id, date}) => (
+const Info = ({id, date, name}) => (
     <div className="episode-tile-info">
         <AiringDate
             date={date}
         />
         <EpisodeNumber
             number={id}
+        />
+        <Name
+            name={name}
         />
         <EpisodeImage
             episodeNumber={id}
