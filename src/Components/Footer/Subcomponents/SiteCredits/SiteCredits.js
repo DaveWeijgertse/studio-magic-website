23872a37aa1d8010135f96c0188885c25eef5883
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 15:09:39 +0100

    Add the credit for the site in the <SiteCredits>

--- src/Components/Footer/Subcomponents/SiteCredits/SiteCredits.js
+++ src/Components/Footer/Subcomponents/SiteCredits/SiteCredits.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import i18n from 'i18n-react';
+
 
 const SiteCredits = () => (
-    <div/>
+    <div className="footer-site-credits-container">
+        {i18n.translate('site-credits')}
+    </div>
 );
 
 export default SiteCredits;
