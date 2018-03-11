7970b0f7ee1fdfec2123dd714382554f94b0d8fe
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 14:50:02 +0100

    Include the <LinksToSocialMedia> in the <Footer>

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 // Subcomponent imports
+import CopyrightInfo from './Subcomponents/CopyrightInfo';
 import LinksToEpisodes from './Subcomponents/LinksToEpisodes';
+
 // Styling import
 import './Footer.scss';
 
@@ -10,6 +12,7 @@ const Footer = () => (
     <div className="footer-container">
         <div className="footer-page-container">
             <LinksToEpisodes />
+            <LinksToSocialMedia />
         </div>
     </div>
 );
