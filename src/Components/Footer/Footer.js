8d0f5089d85af3e09da8c637a2b3ae2f2e551bf1
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 14:59:46 +0100

    Fix bug where the <LinksToSocialMedia> wasn't imported

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 // Subcomponent imports
 import CopyrightInfo from './Subcomponents/CopyrightInfo';
 import LinksToEpisodes from './Subcomponents/LinksToEpisodes';
+import LinksToSocialMedia from './Subcomponents/LinksToSocialMedia';
 
 // Styling import
 import './Footer.scss';
