88e0c3beb0e3aaf9bfebfb4e9bdff602acb0068b
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 02:23:15 +0100

    Add empty component with state for the <EarlierEpisodes>

---
+++ src/Pages/Landing/Subcomponents/EarlierEpisodes/EarlierEpisodes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class EarlierEpisodes extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div className="earlier-episodes-container">
+
+            </div>
+        )
+    }
+}
+
+export default EarlierEpisodes;
