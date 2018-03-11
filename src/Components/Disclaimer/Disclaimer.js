2e0e1222d5979f7672ffe77b3113936f53f1720c
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 15:41:17 +0100

    Add the legal disclaimer text for Magic: the Gathering

--- src/Components/Disclaimer/Disclaimer.js
+++ src/Components/Disclaimer/Disclaimer.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 
 const Disclaimer = () => (
-    <div className="disclaimer"></div>
+    <div className="disclaimer">
+        <div>Wizards of the Coast, Magic: The Gathering, and their logos are trademarks of Wizards of the Coast LLC in the United States and other countries. © 2018 Wizards. All Rights Reserved.</div>
+        <div>Magic: the Gathering, the mana symbols and any other official game images including card art remain the property of Hasbro/Wizards of the Coast. We lay no claim of ownership to them. Any use of Magic: the Gathering property including but not limited to card images, logos, or symbols, are for illustrative purposes only and use of such is not to be construed as any attempt to claim any official approval, support, or sponsorship from Wizards of the Coast.</div>
+    </div>
 );
 
 export default Disclaimer;
