47a173a7cbcaaa856c7d418b5f3be0ca31bb9d6c
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 12:36:47 +0100

    Add the introduction to the <PersonTile>

--- src/Pages/Landing/Subcomponents/WhoAreWe/Subcomponents/PersonTile/PersonTile.js
+++ src/Pages/Landing/Subcomponents/WhoAreWe/Subcomponents/PersonTile/PersonTile.js
@@ -6,11 +6,15 @@ import './PersonTile.scss';
 
 const PersonTile = ({data}) => {
     const shortName    = data.info.name.short;
+    const introduction = data.whoAmI.introduction;
 
     return (
         <div className="person-tile-container">
             <div className="person-name">
                 {shortName}
+            </div>
+            <div className="person-introduction">
+                {introduction}
             </div>
         </div>
     );
