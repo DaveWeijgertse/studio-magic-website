ceb6fc847f2e7f24b2e4bb6741766f9bc9822d43
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 15:13:18 +0100

    Add a link to the GItHub page in the <SiteCredits>

--- src/Components/Footer/Subcomponents/SiteCredits/SiteCredits.js
+++ src/Components/Footer/Subcomponents/SiteCredits/SiteCredits.js
@@ -4,7 +4,12 @@ import i18n from 'i18n-react';
 
 const SiteCredits = () => (
     <div className="footer-site-credits-container">
-        {i18n.translate('site-credits')}
+        <div>{i18n.translate('site-credits')}</div>
+        <a
+            className="link-to-github"
+            href="https://github.com/DaveWeijgertse/studio-magic-website"
+            target="_blank"
+        />
     </div>
 );
 
