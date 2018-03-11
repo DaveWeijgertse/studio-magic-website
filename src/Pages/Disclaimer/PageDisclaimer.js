a93e18edb7ce7f1fa9417b40a27ef7fe2827bcaa
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 20:35:21 +0100

    Add a header in the <PageDisclaimer>

--- src/Pages/Disclaimer/PageDisclaimer.js
+++ src/Pages/Disclaimer/PageDisclaimer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import i18n from 'i18n-react';
 
 // Styling import
 import './Disclaimer.scss';
@@ -6,6 +7,7 @@ import './Disclaimer.scss';
 
 const PageDisclaimer = () => (
     <div className="page-disclaimer">
+        <h1>{i18n.translate('disclaimer')}</h1>
         <div>Wizards of the Coast, Magic: The Gathering, and their logos are trademarks of Wizards of the Coast LLC in the United States and other countries. © 2018 Wizards. All Rights Reserved.</div>
         <div>Magic: the Gathering, the mana symbols and any other official game images including card art remain the property of Hasbro/Wizards of the Coast. We lay no claim of ownership to them. Any use of Magic: the Gathering property including but not limited to card images, logos, or symbols, are for illustrative purposes only and use of such is not to be construed as any attempt to claim any official approval, support, or sponsorship from Wizards of the Coast.</div>
     </div>
