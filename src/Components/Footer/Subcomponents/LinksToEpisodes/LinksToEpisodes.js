af67db53886b134eabf1eac925182ae50a05ba95
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 14:27:38 +0100

    Add a link to the <PageEpisodesList> in the <LinksToEpisodes>

--- src/Components/Footer/Subcomponents/LinksToEpisodes/LinksToEpisodes.js
+++ src/Components/Footer/Subcomponents/LinksToEpisodes/LinksToEpisodes.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import i18n from 'i18n-react';
+import { Link } from 'react-router-dom';
+
+import {
+    ROUTE_EPISODES_LIST,
+} from 'appUtils/routes/paths';
+
 
 const LinksToEpisodes = () => (
     <div>
         <div className="footer-header">
             {i18n.translate('episode.plural')}
         </div>
+        <Link to={ROUTE_EPISODES_LIST}>
+            {i18n.translate('episode.all')}
+        </Link>
     </div>
 );
 
