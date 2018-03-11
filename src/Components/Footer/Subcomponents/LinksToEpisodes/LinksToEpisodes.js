364adcf83f00829776343c058423263eecc1dcd2
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 14:27:12 +0100

    Add a header in the <LinksToEpisodes>

--- src/Components/Footer/Subcomponents/LinksToEpisodes/LinksToEpisodes.js
+++ src/Components/Footer/Subcomponents/LinksToEpisodes/LinksToEpisodes.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import i18n from 'i18n-react';
 
 const LinksToEpisodes = () => (
-    <div/>
+    <div>
+        <div className="footer-header">
+            {i18n.translate('episode.plural')}
+        </div>
+    </div>
 );
 
 export default LinksToEpisodes;
