c02158243cda978472a4a1e4c4790d043f6dd610
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 13:34:16 +0100

    Add a title for the <Contact>

--- src/Pages/Landing/Subcomponents/Contact/Contact.js
+++ src/Pages/Landing/Subcomponents/Contact/Contact.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import i18n from 'i18n-react';
+
 
 
 const Contact = () => (
     <div className="contact-container">
+        <h1>{i18n.translate('contact.title')}</h1>
     </div>
 );
 
