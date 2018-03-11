71cdec650eafb10960d93eb2803e886af4cf5458
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 15:08:53 +0100

    Decrease the font-size and make the color gray in the <CopyRightInfo>

--- src/Components/Footer/Subcomponents/CopyrightInfo/CopyrightInfo.js
+++ src/Components/Footer/Subcomponents/CopyrightInfo/CopyrightInfo.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import i18n from 'i18n-react';
 
+// Styling import
+import './CopyrightInfo.scss';
+
 
 const CopyrightInfo = () => (
     <div className="copyright-info-container">
