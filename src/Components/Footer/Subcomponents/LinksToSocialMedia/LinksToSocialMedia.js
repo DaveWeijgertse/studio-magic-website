a0ac5ccf70a4eb13c16b123e517ad4106ce06e4b
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 14:49:06 +0100

    Include a header for the <LinksToSocialMedia>

--- src/Components/Footer/Subcomponents/LinksToSocialMedia/LinksToSocialMedia.js
+++ src/Components/Footer/Subcomponents/LinksToSocialMedia/LinksToSocialMedia.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import i18n from 'i18n-react';
 
 // Subcomponents import
 import SocialMediaButton from './Subcomponents/SocialMediaButton';
@@ -6,6 +7,9 @@ import SocialMediaButton from './Subcomponents/SocialMediaButton';
 
 const LinksToSocialMedia = () => (
     <div>
+        <div className="footer-header">
+            {i18n.translate('social-media')}
+        </div>
         <SocialMediaButton type="facebook" />
         <SocialMediaButton type="twitter" />
     </div>
