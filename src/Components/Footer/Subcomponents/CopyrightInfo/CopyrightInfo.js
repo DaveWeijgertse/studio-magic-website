52e3a04a087ab0faf7890ff16867eb9187e1b8d6
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 14:58:58 +0100

    Add the copyright info in <CopyrightInfo>

--- src/Components/Footer/Subcomponents/CopyrightInfo/CopyrightInfo.js
+++ src/Components/Footer/Subcomponents/CopyrightInfo/CopyrightInfo.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import i18n from 'i18n-react';
+
 
 const CopyrightInfo = () => (
-    <div/>
+    <div>
+        <div>{i18n.translate('copyright')}</div>
+        <div>{i18n.translate('studio-magic')}</div>
+    </div>
 );
 
 export default CopyrightInfo;
