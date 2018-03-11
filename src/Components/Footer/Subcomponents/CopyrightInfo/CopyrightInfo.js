98f46fc5344995a97e05aa99efb56f6827a0d5d5
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-11 15:08:28 +0100

    Display the text in the <CopyRightInfo> next to each other

--- src/Components/Footer/Subcomponents/CopyrightInfo/CopyrightInfo.js
+++ src/Components/Footer/Subcomponents/CopyrightInfo/CopyrightInfo.js
@@ -3,9 +3,8 @@ import i18n from 'i18n-react';
 
 
 const CopyrightInfo = () => (
-    <div>
-        <div>{i18n.translate('copyright')}</div>
-        <div>{i18n.translate('studio-magic')}</div>
+    <div className="copyright-info-container">
+        {i18n.translate('copyright')} {i18n.translate('studio-magic')}
     </div>
 );
 
