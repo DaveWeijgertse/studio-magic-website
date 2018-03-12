a2086b9cb98f069fa98cca61ae371c5f85944d4b
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-12 19:58:59 +0100

    Change the <PageDisclaimer> in a component with a state

--- src/Pages/Disclaimer/PageDisclaimer.js
+++ src/Pages/Disclaimer/PageDisclaimer.js
@@ -5,12 +5,16 @@ import i18n from 'i18n-react';
 import './Disclaimer.scss';
 
 
-const PageDisclaimer = () => (
-    <div className="page-disclaimer">
-        <h1>{i18n.translate('disclaimer')}</h1>
-        <div>Wizards of the Coast, Magic: The Gathering, and their logos are trademarks of Wizards of the Coast LLC in the United States and other countries. © 2018 Wizards. All Rights Reserved.</div>
-        <div>Magic: the Gathering, the mana symbols and any other official game images including card art remain the property of Hasbro/Wizards of the Coast. We lay no claim of ownership to them. Any use of Magic: the Gathering property including but not limited to card images, logos, or symbols, are for illustrative purposes only and use of such is not to be construed as any attempt to claim any official approval, support, or sponsorship from Wizards of the Coast.</div>
-    </div>
-);
+class PageDisclaimer extends React.Component {
+    render() {
+        return (
+            <div className="page-disclaimer">
+                <h1>{i18n.translate('disclaimer')}</h1>
+                <div>Wizards of the Coast, Magic: The Gathering, and their logos are trademarks of Wizards of the Coast LLC in the United States and other countries. © 2018 Wizards. All Rights Reserved.</div>
+                <div>Magic: the Gathering, the mana symbols and any other official game images including card art remain the property of Hasbro/Wizards of the Coast. We lay no claim of ownership to them. Any use of Magic: the Gathering property including but not limited to card images, logos, or symbols, are for illustrative purposes only and use of such is not to be construed as any attempt to claim any official approval, support, or sponsorship from Wizards of the Coast.</div>
+            </div>
+        );
+    }
+}
 
 export default PageDisclaimer;
