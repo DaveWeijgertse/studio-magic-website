ec1c74f76eb9feda47b13e821a3f95b92de381bc
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-12 20:13:13 +0100

    Add a link to the RSS feed in the <RssFeedSource>

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/Subcomponents/RssFeedSource/RssFeedSource.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/Subcomponents/RssFeedSource/RssFeedSource.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import {
+    RSS
+} from '../../utils/podcastSourceTypes';
+
+// Utils import
+import getPodcastSourceHref from '../../utils/getPodcastSourceHref';
+
 
 const RssFeedSource = ({source}) => (
     <div className="rss-feed-source-container">
+        <a
+            href={getPodcastSourceHref(RSS)}
+            rel="noopener noreferrer"
+            target="_blank"
+        >
+        </a>
     </div>
 );
 
