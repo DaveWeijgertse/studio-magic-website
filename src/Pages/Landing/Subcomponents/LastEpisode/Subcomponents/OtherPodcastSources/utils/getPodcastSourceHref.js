0a2a80261157023def2c2c880f66b8b4bb9e4c1a
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-12 20:12:34 +0100

    Add the href to the RSS feed in the getPodcastSource() util helper

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/utils/getPodcastSourceHref.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/utils/getPodcastSourceHref.js
@@ -2,13 +2,15 @@ import {
     ITUNES,
     STITCHER,
     TUNEIN,
+    RSS,
 } from './podcastSourceTypes';
 
 export default function getPodcastSourceHref(type) {
     const podcastSourceHref = {
         [ITUNES]:   "https://itunes.apple.com/us/podcast/studio-magic/id1309473851",
         [STITCHER]: "https://www.stitcher.com/podcast/studio-magic",
-        [TUNEIN]:   "https://tunein.com/radio/Studio-Magic-p1085711"
+        [TUNEIN]:   "https://tunein.com/radio/Studio-Magic-p1085711",
+        [RSS]:      "https://www.buzzsprout.com/133608.rss"
     }
 
     return podcastSourceHref[type];
--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/utils/podcastSourceTypes.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/utils/podcastSourceTypes.js
@@ -1,3 +1,4 @@
 export const ITUNES   = 'itunes';
 export const STITCHER = 'stitcher';
-export const TUNEIN   = 'tunein'
+export const TUNEIN   = 'tunein';
+export const RSS      = 'rss';
