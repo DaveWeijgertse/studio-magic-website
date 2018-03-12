35bd3c70bbc271fb16a31b79418b35c8d7b8f88e
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-12 20:16:28 +0100

    Add styling to the <RssFeedSource> to make it look like the other <PodcastSources>

--- src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/Subcomponents/RssFeedSource/RssFeedSource.js
+++ src/Pages/Landing/Subcomponents/LastEpisode/Subcomponents/OtherPodcastSources/Subcomponents/RssFeedSource/RssFeedSource.js
@@ -7,10 +7,14 @@ import {
 // Utils import
 import getPodcastSourceHref from '../../utils/getPodcastSourceHref';
 
+// Styling import
+import './RssFeedSource.scss';
+
 
 const RssFeedSource = ({source}) => (
     <div className="rss-feed-source-container">
         <a
+            className="source-rss-feed"
             href={getPodcastSourceHref(RSS)}
             rel="noopener noreferrer"
             target="_blank"
