b6c2593079090c5f1285e5857613fecc1b3a4075
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-17 22:55:36 +0100

    Render each part of the description in the <Description>

--- src/Pages/EpisodeDetail/Subcomponents/Description/Description.js
+++ src/Pages/EpisodeDetail/Subcomponents/Description/Description.js
@@ -3,8 +3,18 @@ import React from 'react';
 // Styling import
 import './Description.scss';
 
-const Description = () => (
+
+const Description = ({ description }) => (
     <div className="episode-detail-container-description">
+        {description.map(
+            (descriptionPart) => {
+                return (
+                    <div className="description-part">
+                        {descriptionPart}
+                    </div>
+                );
+            }
+        )}
     </div>
 );
 
