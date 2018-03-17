7d65fb09565469e76a869bf29d37977e42ed91e1
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-17 22:54:59 +0100

    Give each component in the <PageEpisodeDetail> the container styling

--- src/Pages/EpisodeDetail/Subcomponents/Links/Links.js
+++ src/Pages/EpisodeDetail/Subcomponents/Links/Links.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Styling import
+import './Links.scss';
+
 
 const Links = () => (
     <div className="episode-detail-container-links">
--- src/Pages/EpisodeDetail/Subcomponents/Schedule/Schedule.js
+++ src/Pages/EpisodeDetail/Subcomponents/Schedule/Schedule.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Styling import
+import './Schedule.scss';
+
 
 const Schedule = () => (
     <div className="episode-detail-container-schedule">
