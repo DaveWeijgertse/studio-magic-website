35e8bc97d8062b7a7aeccf547df47018e825fff0
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-17 22:56:34 +0100

    Include the <Description> in the <PageEpisodeDetail>

--- src/Pages/EpisodeDetail/PageEpisodeDetail.js
+++ src/Pages/EpisodeDetail/PageEpisodeDetail.js
@@ -4,13 +4,24 @@ import { withRouter } from 'react-router';
 // Data imports
 import { episodes } from 'appUtils/data/episodes.json';
 
+// Subcomponent imports
+import Description from './Subcomponents/Description';
+
 
 class PageEpisodeDetail extends React.Component {
     render() {
         const episodeId   = this.props.match.params.episodeId;
         const episodeData = episodes[episodeId - 1];
+
+        const {
+            description,
+        } = episodeData;
+
         return (
             <div className="page-episode-detail">
+                <Description
+                    description={description}
+                />
             </div>
         );
     }
