dc1d6f792ac3fa69a6578e47e33e3b963ba9f083
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-18 21:15:34 +0100

    Render the credits for the presentation and editing in the <Credits>

--- src/Pages/EpisodeDetail/Subcomponents/Credits/Credits.js
+++ src/Pages/EpisodeDetail/Subcomponents/Credits/Credits.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import i18n from 'i18n-react';
 
 
-const Credits = () => {
+// @todo: move the different credit parts to seperate subcomponent files
+const Credits = ({ credits }) => {
+    const {
+        editing,
+        presentation,
+    } = credits;
+
     return (
         <div className="episode-detail-container-credits">
-
+            <div className="credit-container">
+                <div className="credits-type">
+                    {i18n.translate('credits.editing')}
+                </div>
+                <div className="credits-names">
+                    {editing}
+                </div>
+            </div>
+            <div className="credit-container">
+                <div className="credits-type">
+                    {i18n.translate('credits.presentation')}
+                </div>
+                <div className="credits-names">
+                    {presentation}
+                </div>
+            </div>
         </div>
     );
 }
