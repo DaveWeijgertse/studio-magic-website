6c504a3829c43c6d16ea69148d98f62109fbd883
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-18 21:31:22 +0100

    Copy the <PlayButton> component for the <EpisodeInfo> in the <PageEpisodeDetail>

---
+++ src/Pages/EpisodeDetail/Subcomponents/EpisodeInfo/Subcomponents/PlayButton/PlayButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import i18n from 'i18n-react';
+
+// Styling imports
+import './PlayButton.scss';
+
+// @todo: remove duplicate component
+const PlayButton = ({id, playingEpisodeId, playEpisodeInEpisodeBar, onStopPlayingEpisode}) => {
+    const isEpisodeCurrentlyPlaying = id === playingEpisodeId;
+
+    return (
+        <div className="play-button-container">
+            <div
+                className={"play-button " + (isEpisodeCurrentlyPlaying ? "stop-playing" : "start-playing")}
+                onClick={() => isEpisodeCurrentlyPlaying ? onStopPlayingEpisode() : playEpisodeInEpisodeBar(id)}
+            >
+                {isEpisodeCurrentlyPlaying ? i18n.translate('stop-listening') : i18n.translate('listen')}
+            </div>
+        </div>
+    );
+}
+
+export default PlayButton;
