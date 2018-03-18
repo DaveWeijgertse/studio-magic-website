562e248b6237a6ac6ed8dc112d66bb0494e5cdc5
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-18 21:14:29 +0100

    Add an empty subcomponent file for the <Credits>

---
+++ src/Pages/EpisodeDetail/Subcomponents/Credits/Credits.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Credits = () => {
+    return (
+        <div className="episode-detail-container-credits">
+
+        </div>
+    );
+}
+
+export default Credits;
