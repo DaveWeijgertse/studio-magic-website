ffbf236e94db88e8c1bba24ae795b364cc94a4fc
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-18 19:51:32 +0100

    Add a header in <Links>

--- src/Pages/EpisodeDetail/Subcomponents/Links/Links.js
+++ src/Pages/EpisodeDetail/Subcomponents/Links/Links.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import i18n from 'i18n-react';
 
 // Styling import
 import './Links.scss';
@@ -6,6 +7,7 @@ import './Links.scss';
 
 const Links = ({ links }) => (
     <div className="episode-detail-container-links">
+        <h1>{i18n.translate('links.title')}</h1>
         <div className="links-container">
             {links.map(
                 (link) => {
