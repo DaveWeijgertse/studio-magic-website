9d82d13ae6726d6ee1798f78517ab5e79e0159bd
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-24 17:57:20 +0100

    Add a link to the <PageEpisodeList> in the <MobileNavigationMenu>

--- src/Components/MobileNavigationMenu/MobileNavigationMenu.js
+++ src/Components/MobileNavigationMenu/MobileNavigationMenu.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import i18n from 'i18n-react';
+import { Link } from 'react-router-dom';
+
+import {
+    ROUTE_EPISODES_LIST,
+} from 'appUtils/routes/paths';
+
 // Library imports
 import { slide as Menu } from 'react-burger-menu';
 
@@ -12,6 +19,9 @@ const MobileNavigationMenu = () => (
             right
             width={225}
         >
+            <Link to={ROUTE_EPISODES_LIST}>
+                {i18n.translate('episode.plural')}
+            </Link>
         </Menu>
     </div>
 );
