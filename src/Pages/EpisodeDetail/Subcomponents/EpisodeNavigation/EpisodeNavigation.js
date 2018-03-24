6c33b9a227e7b666cfbffabd9be1f656a18a13fd
Dave Weijgertse <dave.weijgertse@gmail.com>
2018-03-24 16:38:42 +0100

    Render the <PreviousEpisodeButton>, if there is one

--- src/Pages/EpisodeDetail/Subcomponents/EpisodeNavigation/EpisodeNavigation.js
+++ src/Pages/EpisodeDetail/Subcomponents/EpisodeNavigation/EpisodeNavigation.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 // Subcomponent imports
 import NextEpisodeButton from './Subcomponents/NextEpisodeButton';
+import PreviousEpisodeButton from './Subcomponents/PreviousEpisodeButton';
 
 
 const EpisodeNavigation = ({ currentEpisode, totalEpisodes }) => {
+    const isFirstEpisode = currentEpisode === 1;
     const isLastEpisode  = currentEpisode === totalEpisodes;
 
     return (
@@ -12,6 +14,11 @@ const EpisodeNavigation = ({ currentEpisode, totalEpisodes }) => {
             {!isLastEpisode && (
                 <NextEpisodeButton
                     nextEpisode={currentEpisode + 1}
+                />
+            )}
+            {!isFirstEpisode && (
+                <PreviousEpisodeButton
+                    previousEpisode={currentEpisode - 1}
                 />
             )}
         </div>
