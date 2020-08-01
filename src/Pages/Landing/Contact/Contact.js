cbac9e0a36a47dbdd5519870d192c608b38026e4
Dave Weijgertse <dave.weijgertse@gmail.com>
2020-08-01 11:14:33 +0200

    Home page: Add email address to the contact lane + redesign the lane

--- src/Pages/Landing/Contact/Contact.js
+++ src/Pages/Landing/Contact/Contact.js
@@ -13,7 +13,9 @@ const Contact = () => (
             <h1>{i18n.translate('contact.title')}</h1>
             <div className="contact-links">
                 <div className="contact-links-text">{i18n.translate('contact.via')}</div>
-                <div className="contact-links-container">
+                <div className="contact-links-email">{i18n.translate('contact.email')}</div>
+                <div className="contact-links-divider">{i18n.translate('or').toLowerCase()}</div>
+                <div className="contact-links-social">
                     <ContactButton type="facebook" />
                     <ContactButton type="twitter" />
                 </div>
