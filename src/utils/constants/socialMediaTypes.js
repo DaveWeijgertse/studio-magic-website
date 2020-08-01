c7901c8093235ac9f7721e870bfe107d15fcb267
Dave Weijgertse <dave.weijgertse@gmail.com>
2020-08-01 11:22:03 +0200

    Social: Add URLs to Instagram and LinkedIn

--- src/utils/constants/socialMediaTypes.js
+++ src/utils/constants/socialMediaTypes.js
@@ -1,2 +1,4 @@
+export const INSTAGRAM = 'instagram';
 export const FACEBOOK = 'facebook';
 export const TWITTER = 'twitter';
+export const LINKEDIN = 'linkedin';
--- src/utils/helpers/getSocialMediaHref.js
+++ src/utils/helpers/getSocialMediaHref.js
@@ -1,9 +1,11 @@
-import { FACEBOOK, TWITTER } from 'utils/constants/socialMediaTypes';
+import { INSTAGRAM, FACEBOOK, TWITTER, LINKEDIN } from 'utils/constants/socialMediaTypes';
 
 export default function getSocialMediaHref(type) {
     const socialMediaHref = {
+        [INSTAGRAM]: 'https://www.instagram.com/studiomagicpodcast',
         [FACEBOOK]: 'https://www.facebook.com/studiomagicnl',
         [TWITTER]: 'https://twitter.com/StudioMagicNL',
+        [LINKEDIN]: 'https://www.linkedin.com/company/studiomagicnl',
     };
 
     return socialMediaHref[type];
