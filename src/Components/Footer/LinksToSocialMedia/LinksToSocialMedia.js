f9761d7af6b216c11b1293b7d473f68a090811ea
Dave Weijgertse <dave.weijgertse@gmail.com>
2020-08-01 11:28:04 +0200

    Footer: Add email address to the social links

--- src/Components/Footer/LinksToSocialMedia/LinksToSocialMedia.js
+++ src/Components/Footer/LinksToSocialMedia/LinksToSocialMedia.js
@@ -4,9 +4,13 @@ import i18n from 'i18n-react';
 // Subcomponents import
 import SocialMediaButton from './SocialMediaButton';
 
+// Styling import
+import './LinksToSocialMedia.scss';
+
 const LinksToSocialMedia = () => (
-    <div>
+    <div class="footer-links-to-social-media">
         <div className="footer-header">{i18n.translate('social-media')}</div>
+        <div class="footer-links-to-social-media-email">{i18n.translate('contact.email')}</div>
         <SocialMediaButton type="instagram" />
         <SocialMediaButton type="facebook" />
         <SocialMediaButton type="twitter" />
